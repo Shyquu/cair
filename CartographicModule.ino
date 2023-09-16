7e81730aff02603b4d8044df104772be7070f6aa
Leander Larisch <leanderlarisch@Gunters-Mac.fritz.box>
2023-09-16 16:06:40 +0200

    Initial commit

---
+++ CartographicModule.ino
@@ -0,0 +1,22 @@
+// Leander 
+// Kartographisches Modul zum ansteuern der Ultraschallsensoren
+
+void initSensors() {
+  // muss noch hinzugefügt werden
+  pinMode();
+}
+
+void processGyroInput() {
+
+}
+
+// Wird ausgeführt wenn das Board angeschaltet wird, wird nur einmal ausgeführt 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
