3f3dcff567d6d35196303d1af3bd27d20515a467
Leander Larisch <leanderlarisch@Gunters-Mac.fritz.box>
2023-09-16 16:24:09 +0200

    Added distance measurement-functionality

--- CartographicModule.ino
+++ CartographicModule.ino
@@ -1,22 +1,52 @@
 // Leander 
-// Kartographisches Modul zum ansteuern der Ultraschallsensoren
+// Kartographisches Modul zum Ansteuern der Ultraschallsensoren
 
-void initSensors() {
-  // muss noch hinzugefügt werden
-  pinMode();
-}
+#define TRIGGER_PIN_1 2 // Pin für den Trigger des ersten Sensors
+#define ECHO_PIN_1 3    // Pin für den Echo des ersten Sensors
+#define TRIGGER_PIN_2 4 // Pin für den Trigger des zweiten Sensors
+#define ECHO_PIN_2 5    // Pin für den Echo des zweiten Sensors
 
-void processGyroInput() {
-
-}
-
-// Wird ausgeführt wenn das Board angeschaltet wird, wird nur einmal ausgeführt 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialisiere die Serielle Kommunikation für Debugging
+  Serial.begin(9600);
 
+  // Konfiguriere die Pins für Trigger und Echo als Ein- bzw. Ausgänge
+  pinMode(TRIGGER_PIN_1, OUTPUT);
+  pinMode(ECHO_PIN_1, INPUT);
+  pinMode(TRIGGER_PIN_2, OUTPUT);
+  pinMode(ECHO_PIN_2, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Funktion zur Entfernungsmessung aufrufen
+  float distance1 = measureDistance(TRIGGER_PIN_1, ECHO_PIN_1);
+  float distance2 = measureDistance(TRIGGER_PIN_2, ECHO_PIN_2);
 
+  // Die gemessenen Entfernungen auf der Seriellen Schnittstelle ausgeben
+  Serial.print("Entfernung Sensor 1: ");
+  Serial.print(distance1);
+  Serial.println(" cm");
+  
+  Serial.print("Entfernung Sensor 2: ");
+  Serial.print(distance2);
+  Serial.println(" cm");
+  
+  delay(1000); // Eine Sekunde warten, bevor die nächste Messung durchgeführt wird
+}
+
+float measureDistance(int triggerPin, int echoPin) {
+  // Sende einen 10us langen Ultraschall-Impuls aus
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  // Messe die Dauer des Echo-Impulses in Mikrosekunden
+  long duration = pulseIn(echoPin, HIGH);
+
+  // Berechne die Entfernung in Zentimetern anhand der Schallgeschwindigkeit (343 m/s)
+  float distance = (float)duration * 0.0343 / 2;
+
+  return distance;
 }
